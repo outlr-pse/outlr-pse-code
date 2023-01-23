bc226466b0e587254dc678c8261ed8d2f26a8e4f
Salomo Hummel <salomo.hummel@student.kit.edu>
2023-01-23 17:23:30 +0100

    more model

---
+++ backend/models/odm/hyper_parameter.py
@@ -0,0 +1,17 @@
+from models.base import Base
+from sqlalchemy import Column, Integer, Text
+from sqlalchemy.orm import relationship
+
+
+class HyperParameter(Base):
+    __tablename__ = 'hyper_parameter'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(Text)
+    odm_id = Column(Integer, ForeignKey('odm.id'))
+    odm = relationship("ODM", back_populates="hyper_parameters")
+
+    def to_json(self) -> dict:
+        return {
+            "id": self.id,
+            "name": self.name
+        }

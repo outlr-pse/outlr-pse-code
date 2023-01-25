d65049f584ee4b653aa9a9abb31a02df8d956b07
Salomo Hummel <salomo.hummel@student.kit.edu>
2023-01-25 14:03:40 +0100

    add JWTManager

--- backend/src/api/api.py
+++ backend/src/api/api.py
@@ -2,11 +2,14 @@ from flask import Flask, jsonify
 from api.experiment_api import experiment_api
 from api.user_management_api import user_management_api
 from api.odm_api import odm_api
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.register_blueprint(experiment_api)
 app.register_blueprint(user_management_api)
 app.register_blueprint(odm_api)
+
+JWTManager(app)
 
 
 @app.route('/status', methods=['GET'])

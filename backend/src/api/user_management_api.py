e21a41ea321d0420b163a415c7493c6b116a3f7b
Udo Zucker <email@udozucker.de>
2023-01-28 11:56:05 +0100

    very basic user_management mock api

--- backend/src/api/user_management_api.py
+++ backend/src/api/user_management_api.py
@@ -7,29 +7,33 @@ Endpoints defined:
     /user/check-token
 """
 
-from flask import Blueprint, Response
+from flask import Blueprint, Response, jsonify, request
 from flask_jwt_extended import jwt_required
+
+from api.api import mock_jwt_required
 
 user_management_api = Blueprint('user_management', __name__, url_prefix='/user')
 
 
 @user_management_api.route('/login', methods=['POST'])
 def login() -> Response:
-    return Response(status=501)
+    username = request.json['username']
+    return jsonify(message=f'Successfully logged in as {username}', status=200)
 
 
 @user_management_api.route('/register', methods=['POST'])
 def register() -> Response:
-    return Response(status=501)
+    username = request.json['username']
+    return jsonify(message=f'User {username} registered', status=200)
 
 
 @user_management_api.route('/logout', methods=['POST'])
-@jwt_required()
+@mock_jwt_required()
 def logout() -> Response:
-    return Response(status=501)
+    return jsonify(message="Token deleted", status=200)
 
 
 @user_management_api.route('/check-token', methods=['POST'])
-@jwt_required()
+@mock_jwt_required()
 def check_token() -> Response:
-    return Response('Token is valid')
+    return jsonify(message="Token is valid", status=200)

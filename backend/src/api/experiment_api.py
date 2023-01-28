79901daaceb6d36d6d091a8f33148ef4a7ea0547
Udo Zucker <email@udozucker.de>
2023-01-28 11:49:53 +0100

    added mock files and edited experiment and odm api python files for mock response

--- backend/src/api/experiment_api.py
+++ backend/src/api/experiment_api.py
@@ -8,20 +8,28 @@ Endpoints defined:
     /experiment/create
     /experiment/download-result/<exp_id>
 """
+import json
 
-from flask import Blueprint, Response, jsonify
+from flask import Blueprint, Response, jsonify, send_file
 from flask_jwt_extended import jwt_required
-
-from api.api import mock_jwt_required
-
 experiment_api = Blueprint('experiment', __name__, url_prefix='/experiment')
+
+#to be deleted
+from api.api import mock_jwt_required
+import random
+
 mock_success = True
+# when True get_all returns an empty array
+no_experiments = False
+single_experiment = (not no_experiments) and True
+
+#to be deleted
 
 @experiment_api.route('/validate-dataset', methods=['POST'])
 @mock_jwt_required()
 def validate_dataset() -> Response:
     if mock_success:
-        return jsonify(status=200)
+        return jsonify(message="Valid dataset", status=200)
 
     no_success_response = jsonify(message="Invalid dataset", status=400)
     no_success_response.status = 400
@@ -31,7 +39,7 @@ def validate_dataset() -> Response:
 @mock_jwt_required()
 def validate_ground_truth() -> Response:
     if mock_success:
-        return jsonify(status=200)
+        return jsonify(message="Valid dataset", status=200)
 
     no_success_response = jsonify(message="Invalid dataset", status=400)
     no_success_response.status = 400
@@ -41,23 +49,56 @@ def validate_ground_truth() -> Response:
 @experiment_api.route('/get-result/<int:exp_id>', methods=['GET'])
 @mock_jwt_required()
 def get_result(exp_id: int) -> Response:
-    return Response(status=501)
+    if not mock_success:
+        no_success_response = jsonify(message="Experiment with provided id does not exist", status=404)
+        no_success_response.status = 404
+        return no_success_response
 
-
+    file = open('mock_files/experiment_result.json')
+    exp_result = json.load(file)
+    response = jsonify(experiment=exp_result, message="Experiment successfully retrieved", status=200)
+    return response
 
 @experiment_api.route('/get-all', methods=['GET'])
 @mock_jwt_required()
 def get_all() -> Response:
-    return Response(status=501)
+    if no_experiments:
+        response = jsonify(experiment=[], message="No experiments to be retrieved", status=200)
+        return response
+
+    if single_experiment:
+        experiment_one = json.load(open('mock_files/experiment_one.json'))
+        response = jsonify(experiments=[experiment_one], message="Experiment successfully retrieved", status=200)
+        return response
+
+    experiment_one = json.load(open('mock_files/experiment_one.json'))
+    experiment_two = json.load(open('mock_files/experiment_two.json'))
+    experiments = [experiment_one, experiment_two]
+    experiment_array = [experiment_one, experiment_two]
+
+    for i in range(15):
+        experiment_array.append(experiments[random.randint(0, 1)])
+
+    response = jsonify(experiments=experiment_array, message="Experiment(s) successfully retrieved", status=200)
+    return response
 
 
 @experiment_api.route('/create', methods=['POST'])
 @mock_jwt_required()
 def create() -> Response:
-    return Response(status=501)
+    if not mock_success:
+        no_success_response = jsonify(message="Experiment could not be created - something went wrong", status=400)
+        no_success_response.status = 400
+        return no_success_response
 
+    return jsonify(message="Experiment successfully created", status=200)
 
 @experiment_api.route('/download-result/<int:exp_id>', methods=['GET'])
 @mock_jwt_required()
 def download_result(exp_id: int) -> Response:
-    return Response(status=501)
+    if not mock_success:
+        no_success_response = jsonify(message="Experiment result could not be download - not found", status=404)
+        no_success_response.status = 404
+        return no_success_response
+
+    return send_file("mock_files/outliers.csv", mimetype="text/csv", as_attachment=True)
--- backend/src/api/odm_api.py
+++ backend/src/api/odm_api.py
@@ -4,20 +4,52 @@ Endpoints defined:
     /odm/get-all
     /odm/get-parameters/<odm_id>
 """
+import json
+import random
 
-from flask import Blueprint, Response
+from flask import Blueprint, Response, jsonify
 from flask_jwt_extended import jwt_required
 
-odm_api = Blueprint('odm', __name__, url_prefix='/odm')
+from api.api import mock_jwt_required
 
+odm_api = Blueprint('odm', __name__, url_prefix='/odm')
+mock_success = True
+single_odm = True
 
 @odm_api.route('/get-all', methods=['GET'])
-@jwt_required()
+@mock_jwt_required()
 def get_all() -> Response:
-    return Response(status=501)
+    if not mock_success:
+        no_success_response = jsonify(message="Something went wrong", status=400)
+        no_success_response.status = 400
+        return no_success_response
+
+    if single_odm:
+        odm_one = json.load(open('mock_files/odm_one.json'))
+        response = jsonify(odms=[odm_one], message="ODM successfully retrieved")
+        return response
+
+    odm_one = json.load(open('mock_files/odm_one.json'))
+    odm_two = json.load(open('mock_files/odm_two.json'))
+
+    odms = [odm_one, odm_two]
+    odms_array = [odm_one, odm_two]
+
+    for i in range(15):
+        odms_array.append(odms[random.randint(0, 1)])
+
+    response = jsonify(odms=odms_array, message="ODMs successfully retrieved", status=200)
+    return response
 
 
 @odm_api.route('/get-parameters/<int:odm_id>', methods=['GET'])
-@jwt_required()
+@mock_jwt_required()
 def get_parameters(odm_id: int) -> Response:
-    return Response(status=501)
+    if not mock_success:
+        no_success_response = jsonify(message="ODM not found", status=400)
+        no_success_response.status = 400
+        return no_success_response
+
+    hyperparams = json.load(open('mock_files/hyperparameters.json'))
+    response = jsonify(hyperparameters=hyperparams, message="Hyperparameters successfully retrieved", status=200)
+    return response

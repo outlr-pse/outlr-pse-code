71cbac9e4cb647540c8556f67de7eed32433bbda
Erik Wu <erik.wu@gmx.de>
2023-01-30 00:20:02 +0100

    added font support for icons

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-
+  <h2> <span class ="material-icons md-light">search</span></h2>
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>

128f1b2b3800d71788cf4dc26d101739920d3964
Simeon Schrape <ucfoh@student.kit.edu>
2023-01-29 22:25:57 +0100

    some changes

--- backend/src/database/database_access.py
+++ backend/src/database/database_access.py
@@ -6,21 +6,21 @@ from models.experiment.experiment import Experiment
 from models.odm.odm import ODM
 from models.user.user import User
 
-engine = create_engine("postgresql://postgres:postgres@localhost:5432/postgres")
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/outlr_database")
 Base.metadata.create_all(bind=engine, checkfirst=True)
 Session = sessionmaker(bind=engine)
 session: Session
 
 
-def get_experiment(self, experiment: Experiment):
+def get_experiment(self, experiment: Experiment) -> Experiment:
     return self.session.query(Experiment).filter_by(id=experiment.id).first()
 
 
-def get_experiment(self, id: int):
+def get_experiment(self, id: int) -> Experiment:
     return self.session.query(Experiment).filter_by(id=id).first()
 
 
-def get_user_experiments(self, user: User):
+def get_user_experiments(self, user: User) -> list[Experiment]:
     return self.session.query(Experiment).filter_by(user=user).all()
 
 
@@ -29,7 +29,7 @@ def add_user(self, user: User):
     self.session.commit()
 
 
-def get_user(self, id: int):
+def get_user(self, id: int) -> User:
     return self.session.query(User).filter_by(id=id).first()
 
 
@@ -38,5 +38,5 @@ def add_odm(self, odm: ODM):
     self.session.commit()
 
 
-def get_odm(self, id: int):
+def get_odm(self, id: int) -> ODM:
     return self.session.query(ODM).filter_by(id=id).first()

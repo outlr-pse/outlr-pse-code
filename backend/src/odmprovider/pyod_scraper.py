9ce62e542eed4dc873d21ae5743917ef20a12625
Simeon Schrape <ucfoh@student.kit.edu>
2023-01-31 20:05:30 +0100

    odm provider finished

--- backend/src/odmprovider/pyod_scraper.py
+++ backend/src/odmprovider/pyod_scraper.py
@@ -1,34 +1,19 @@
 import importlib
 
+import pkgutil
+import os.path
+
+import pyod.models
+
 from models.odm.odm import ODM
 from odmprovider.odm_provider import ODMProvider
 
 
 class PyODScraper(ODMProvider):
 
-    def next_odm(self):
-        pyod_module = None
-        try:
-            pyod_module = importlib.import_module("models.odm.odm", "pyod.models")
-        except Exception as e:
-            print(f"Error importing module: {e}")
-        else:
-            for c in dir(pyod_module):
-                print(c)
-                getattr(pyod_module, c)
-                try:
-                    print(pyod_module.__name__)
-                except Exception as e:
-                    print(f"Error: {e}")
-                try:
-                    print(pyod_module.__package__)
-                except Exception as e:
-                    print(f"Error: {e}")
-                try:
-                    print(pyod_module.)
-                except Exception as e:
-                    print(f"Error: {e}")
-                try:
-                    print(pyod_module.odm.__name__)
-                except Exception as e:
-                    print(f"Error: {e}")
+    def next_odm(self) -> ODM:
+        pkgpath = os.path.dirname(pyod.models.__file__)
+        for _, name, _ in pkgutil.iter_modules([pkgpath]):
+            yield ODM(name)
+
+
--- backend/tests/test_pyod_scraper.py
+++ backend/tests/test_pyod_scraper.py
@@ -8,4 +8,14 @@ class MyTestCase(unittest.TestCase):
 
     def test_scraper(self):
         pyod_scraper = PyODScraper()
-        PyODScraper.next_odm(pyod_scraper)
+        odm = pyod_scraper.next_odm()
+        self.assertEqual(next(odm), "abod")
+        self.assertEqual(next(odm), "alad")
+        for i in range(0, 10):
+            next(odm)
+        self.assertEqual(next(odm), "deep_svdd")
+
+        odm = pyod_scraper.next_odm()
+        for o in odm:
+            self.assertIsInstance(o, ODM)
+

e3ac6fcb13b5804436a283433542fd99946d9dc0
Salomo Hummel <salomo.hummel@student.kit.edu>
2023-02-02 20:08:55 +0100

    Remove wrong changes.

---
+++ frontend/src/components/basic/BaseTable.vue
@@ -0,0 +1,27 @@
+<template>
+    <table :style="style">
+      <thead>
+      <slot name="header"></slot>
+      </thead>
+      <tbody>
+      <slot name="body"></slot>
+      </tbody>
+    </table>
+</template>
+
+<script lang="ts">
+import {CSSProperties, defineComponent} from "vue";
+
+export default defineComponent({
+
+  props: {
+    style: {
+      type: Object as () => CSSProperties,
+      required: false,
+    },
+  },
+})
+</script>
+
+<style scoped>
+</style>

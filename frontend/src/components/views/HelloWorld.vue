acd85e637d7a750488d842f8873bdaa42264d26a
Salomo Hummel <salomo.hummel@student.kit.edu>
2023-02-02 20:11:07 +0100

    Remove more wrong changes.

--- frontend/src/components/views/HelloWorld.vue
+++ frontend/src/components/views/HelloWorld.vue
@@ -31,7 +31,8 @@ const count = ref(0)
   <h1>{{ $t('message.greeting') }}, {{$t('message.name')}}</h1>
 
   <div class="card">
-    <button type="button" @click="$i18n.locale = Languages.GERMAN">count is {{ count }}</button>
+    <Button @buttonClick="$i18n.locale = Languages.GERMAN" :button-type="ButtonType.ACTIVE" :size="[150, 50]"
+     :text="'count is: ' + count" />
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR

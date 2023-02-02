0b45912edfe9aeb9d7847c19a6f04532c330e6a2
Udo Zucker <email@udozucker.de>
2023-02-03 00:31:10 +0100

    created ErrorType on frontend

---
+++ frontend/src/models/error/ErrorType.ts
@@ -0,0 +1,14 @@
+/**
+ * This enum represents an ErrorType.
+ * The string that is mapped to each enum entry is used by the API to classify the error
+ */
+export enum ErrorType {
+    /** An error, which has to do with JWT access tokens and operation */
+    JWTAuthError = "JWTAuthError",
+    /** An error in the user management part of the API */
+    UserManagementError = "UserManagementError",
+    /** An error in the experiment part of the API */
+    ExperimentError = "ExperimentError",
+    /** An error in the ODM part of the API */
+    ODMError = "ODMError",
+}

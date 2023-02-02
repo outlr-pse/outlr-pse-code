b49a0a460dd4d776ea60da1765eef2708c104780
Udo Zucker <email@udozucker.de>
2023-02-03 00:21:43 +0100

    added services, vuex setup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import './assets/main.css'
 import App from './App.vue'
 import {i18n} from "./language/LanguageSetup";
+import store from "./store";
 
 const app = createApp(App)
 
 app.use(i18n)
+app.use(store)
 app.mount('#app')

02d6a459b22349eea87db7305601796de24fd42c
Udo Zucker <email@udozucker.de>
2023-02-05 22:34:24 +0100

    implemented all non auth axios wrappers

---
+++ frontend/src/api/DataRetrievalService.ts
@@ -0,0 +1,18 @@
+import {storage} from "./APIRequests";
+import {errorOther} from "./ErrorOther";
+
+export function authHeader() {
+    /**
+     * Generates the header used for sending the token provided with http requests to the backend. If
+     * no token is in the local storage it is a empty JSON.
+     */
+
+    let token = storage.getItem('access_token')
+
+    if (token != null) {
+        return {Authorization: 'Bearer ' + token}
+    }
+
+    return {}
+
+}

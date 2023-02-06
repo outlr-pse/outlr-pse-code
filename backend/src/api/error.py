5ae2561acabcff60428a54e838bd0e2e6dc60ed5
Udo Zucker <email@udozucker.de>
2023-02-06 23:04:28 +0100

    implemented user management api, added custom jwt error messages

--- backend/src/api/error.py
+++ backend/src/api/error.py
@@ -46,16 +46,16 @@ username_already_taken = {
     "status": 409,
 }
 
-token_not_provided = {
-    "error": "token_not_provided",
-    "message": "The provided token is not linked to any User or no token provided",
+token_not_valid = {
+    "error": "token_not_valid",
+    "message": "The provided token is not valid or no token provided",
     "status": 401,
 }
 
-token_not_linked = {
-    "error": "token_not_linked",
-    "message": "The provided token is not linked to any User or no token provided",
-    "status": 401,
+user_look_up_failed = {
+    "error": "user_look_up_failed",
+    "message": "The user look up failed",
+    "status": 400,
 }
 
 dataset_not_valid = {

cac3f3d1e63c4738e923673cbafa225a65587525
Erik Wu <erik.wu@gmx.de>
2023-02-06 14:14:01 +0100

    changed port 1337

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -5,4 +5,4 @@ setup_db()
 
 if __name__ == '__main__':
     # Starts the api in debug mode for development. Do not use in production.
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='localhost', port=1337, debug=True)

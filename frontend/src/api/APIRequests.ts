9cb372219a1e27e623651c9bdef8935b218e0dfc
Erik Wu <erik.wu@gmx.de>
2023-02-06 14:16:37 +0100

    added reminder to change mock to local storage

--- frontend/src/api/APIRequests.ts
+++ frontend/src/api/APIRequests.ts
@@ -6,6 +6,8 @@ import {MockStorage} from "./MockStorage";
 export const axiosClient = axios.create({
     baseURL: 'http://127.0.0.1:1337/api'
 });
+
+//todo
 export const storage = new MockStorage()
 
 export async  function sendLogout() : Promise<any>{

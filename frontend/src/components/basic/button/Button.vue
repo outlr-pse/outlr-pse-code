e197953e103c9acb974db9432d299e6eea0e2ce7
Erik Wu <erik.wu@gmx.de>
2023-02-07 02:51:14 +0100

    finished appearing card

--- frontend/src/components/basic/button/Button.vue
+++ frontend/src/components/basic/button/Button.vue
@@ -45,7 +45,7 @@ export default defineComponent({
       textSize:{
         type: Object as () => [number, CSSProperties['fontWeight']],
         required: false,
-        default: [14, 500],
+        default: [16, 500],
       }
     },
   methods: {

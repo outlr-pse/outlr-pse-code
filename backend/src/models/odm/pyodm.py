e7354052393662c2423fa8576e5aa506d0a3685c
Bennet Hörmann <bennet.hoermann@student.kit.edu>
2023-02-08 20:22:17 +0100

    Update ODM to use DataFrame

--- backend/src/models/odm/pyodm.py
+++ backend/src/models/odm/pyodm.py
@@ -6,18 +6,18 @@ This file contains the PyODM class, which is a wrapper for the PyOD library.
 from typing import Any
 
 import numpy
+import pandas as pd
 
 from models.odm.odm import ODM
-from models.dataset.dataset import Dataset
 import pyod
 import importlib
 
 
 class PyODM(ODM):
-    def run_odm(self, subspace: Dataset, hyper_params: dict[str, Any]) -> numpy.ndarray:
+    def run_odm(self, subspace: pd.DataFrame, hyper_params: dict[str, Any]) -> numpy.ndarray:
         """Runs the ODM on the given subspace
         Args:
-            subspace (Dataset): The subspace to run the ODM on
+            subspace (DataFrame): The subspace to run the ODM on
             hyper_params (dict[str, Any]): The hyper parameters for the ODM
         Returns:
             list[int]: The labels for the outliers on this subspace
@@ -26,5 +26,5 @@ class PyODM(ODM):
         module = importlib.import_module(f'pyod.models.{pyOD_module_name}')
         pyODM_cls = getattr(module, cls_name)
         pyODM = pyODM_cls(**hyper_params)
-        pyODM.fit(subspace.dataset)
+        pyODM.fit(subspace)
         return pyODM.labels_

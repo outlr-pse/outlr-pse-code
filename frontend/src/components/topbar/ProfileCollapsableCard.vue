86ba0ce1664f9d2e9ec4a95b62302c39b3c3b1f6
Simeon Schrape <ucfoh@student.kit.edu>
2023-02-08 13:10:44 +0100

    fixed the profile hover

--- frontend/src/components/topbar/ProfileCollapsableCard.vue
+++ frontend/src/components/topbar/ProfileCollapsableCard.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="profile">
+  <div class="profile" @mouseenter="showCard = true" @mouseleave="showCard = false">
     <div class="circle"/>
     <div class="card-container">
       <img alt="arrow" @click="() => $router.push('/')" class="arrow" src="../../assets/pp_arrow.svg"
            width="13" :style="rotatedImage"/>
-      <div class="card">
-
-        <p style=" margin-top: 16px; margin-bottom: 16px">Signed in as <b>{{ store.getters["auth/username"] }}</b></p>
-        <div class="separator"></div>
-        <a href="./logout">Logout <i class="material-icons md-light"
-                                     style="font-size: 16px; vertical-align: middle; margin-left: 5px">logout</i></a>
-      </div>
-
+      <transition name="fade">
+        <div class="card" v-if="showCard" @mouseleave="showCard = false" @mouseenter="showCard = true">
+          <p style=" margin-top: 16px; margin-bottom: 16px">Signed in as <b>{{ store.getters["auth/username"] }}</b></p>
+          <div class="separator"></div>
+          <a href="./logout">Logout <i class="material-icons md-light"
+                                       style="font-size: 16px; vertical-align: middle; margin-left: 5px">logout</i></a>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
@@ -27,22 +27,21 @@ export default defineComponent({
       rotation: {
         type: Number,
         default: 90
-      }
+      },
+      showCard: false
     }
   },
 
-computed: {
-  store()
-  {
-    return store
-  },
-  rotatedImage() : {transform: string}
-  {
-    return {
-      transform: 'rotate(' + this.rotation + 'deg)'
+  computed: {
+    store() {
+      return store
+    },
+    rotatedImage(): { transform: string } {
+      return {
+        transform: 'rotate(' + this.rotation + 'deg)'
+      }
     }
   }
-}
 })
 </script>
 
@@ -65,28 +64,14 @@ a {
   margin-left: 15px;
 }
 
-
-.profile:hover .card, .card:hover {
-  display: block;
-  position: absolute;
-  opacity: 100;
-  top: calc(var(--top-bar-height) / 2 - 10px);
-  left: -100px;
-  right: 0;
-  bottom: 0;
-  width: 110px;
-  height: 110px;
-  background-color: rgb(47, 41, 58, 0.70);
-  z-index: 100;
-  border-radius: 12px;
-  border: 1px solid var(--color-stroke);
-  font-size: 14px;
-  transition: all 0.25s ease-in-out;
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .8s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 
-
 .card {
-  opacity: 0;
   display: block;
   position: absolute;
   top: calc(var(--top-bar-height) / 2 - 10px);

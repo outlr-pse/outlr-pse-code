cb1348c48468d3fa232b6fa5db96c9f325169015
Simeon Schrape <ucfoh@student.kit.edu>
2023-02-08 21:24:42 +0100

    fixed error

--- frontend/src/components/views/createExperimentView/Component/SubspaceSection.vue
+++ frontend/src/components/views/createExperimentView/Component/SubspaceSection.vue
@@ -10,10 +10,12 @@
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
   name: "SubspaceSection"
-}
+})
 </script>
 
 <style scoped>

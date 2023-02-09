3eb5e9d53be07466eadb8f0f7698d87dd99ec2ad
Udo Zucker <email@udozucker.de>
2023-02-09 16:33:08 +0100

    added eye for login

--- frontend/src/components/views/LoginView/LoginView.vue
+++ frontend/src/components/views/LoginView/LoginView.vue
@@ -9,14 +9,23 @@
         </div>
 
         <div class="login-form">
-            <div class="error" v-show="error">{{errorMessage}}</div>
-            <div class="text-fields">
-              <input v-bind:class="{'errorInput':error}" class="username" @input="usernameInput" @click="() => error = false" v-model="username" placeholder="Username"/>
-              <input v-bind:class="{'errorInput':error}" class="password" @input="passwordInput" @click="() => error = false" v-model="password" placeholder="Password" type="password"/>
+          <div class="error" v-show="error">{{ errorMessage }}</div>
+          <div class="text-fields">
+            <input v-bind:class="{'errorInput':error}" class="username"
+                   @click="() => error = false" v-model="username" placeholder="Username"/>
+            <div style="position:relative">
+              <input v-bind:class="{'errorInput':error}" class="password"
+                     @click="() => error = false" v-model="password" placeholder="Password"
+                     :type="inputTypePassword"/>
+              <span class="material-icons md-dark passwordIcon"
+                    @click="toggleVisibilityPassword">{{visiblePassword}}</span>
             </div>
-            <div class="submit-field">
-              <input v-bind:class="{'validSubmit': validInput, 'errorSubmit':error}" ref="submit" @click="tryLoginSubmit" type="button" value="Continue">
-            </div>
+
+          </div>
+          <div class="submit-field">
+            <input v-bind:class="{'validSubmit': validInput, 'errorSubmit':error}" ref="submit" @click="tryLoginSubmit"
+                   type="button" value="Continue">
+          </div>
         </div>
       </div>
     </Card>
@@ -43,7 +52,9 @@ export default defineComponent({
       password: "",
       passwordRepeated: "",
       error: false,
-      errorMessage: "Something went wrong!"
+      errorMessage: "Something went wrong!",
+      inputTypePassword: "password",
+      visiblePassword: "visibility"
     };
   },
   methods: {
@@ -51,14 +62,19 @@ export default defineComponent({
       this.username = ""
       this.password = ""
     },
+    toggleVisibilityPassword(){
+      this.inputTypePassword = this.inputTypePassword === "password"  ? "text" : "password"
+      this.visiblePassword = this.visiblePassword === "visibility_off" ? "visibility": "visibility_off"
+      this.error = false;
+    },
     async tryLoginSubmit() {
 
       if (!validateUsername(this.username) || !validatePassword(this.password, this.password)) {
-          this.error = true;
-          this.errorMessage = this.$t('message.login-view.errors.provided-credentials-wrong')
-          this.resetInputFields()
-          return;
-        }
+        this.error = true;
+        this.errorMessage = this.$t('message.login-view.errors.provided-credentials-wrong')
+        this.resetInputFields()
+        return;
+      }
 
       const response = await login(this.username, this.password)
       if (response.error != null) {
@@ -74,114 +90,124 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.errorInput{
-  border-color: var(--color-close-button) !important;
-  background: none;
-  color: var(--color-close-button)!important;
+.passwordIcon {
+  float: right;
+  position: absolute;
+  right: 18px;
+  top: 13px;
+  z-index: 90;
+  color: var(--color-main-white);
+  width: 24px;
+  height: 24px;
+  user-select: none;
 }
 
-.error{
-  font-size:18px;
+.errorInput {
+  border-color: var(--color-close-button) !important;
+  background: none;
+  color: var(--color-close-button) !important;
+}
+
+.error {
+  font-size: 18px;
   font-weight: 500;
   border: 2px solid var(--color-close-button);
-  color:var(--color-close-button);
+  color: var(--color-close-button);
   padding: 10px;
   border-radius: 7px;
 }
 
-.errorSubmit{
+.errorSubmit {
   background: var(--color-close-button) !important;
 }
 
-.container{
-  height:100%;
+.container {
+  height: 100%;
   align-items: center;
   display: flex;
   justify-content: center;
 }
 
-.tip{
-  background:var(--color-auth-form-background);
+.tip {
+  background: var(--color-auth-form-background);
   border: none;
   padding: 0;
-  margin:0;
+  margin: 0;
 }
 
 .content {
   width: min(70vw, 325px);
   padding: 30px;
-  display:grid;
+  display: grid;
   grid-template-rows: auto auto;
-  gap:30px;
+  gap: 30px;
 }
 
-.header{
+.header {
   text-align: left;
-  display:grid;
+  display: grid;
   gap: 7px;
 }
 
-.title{
+.title {
   font-size: 22px;
   font-weight: 800;
 }
 
-.subtitle{
-  font-size:20px;
+.subtitle {
+  font-size: 20px;
   font-weight: 700;
   color: var(--color-stroke)
 }
 
-.login-form{
+.login-form {
   display: grid;
   grid-template-rows: auto;
   gap: 30px;
 }
 
-.text-fields{
-  display:grid;
-  gap:15px;
+.text-fields {
+  display: grid;
+  gap: 15px;
 }
 
-.valid{
+.valid {
   border: 2px solid green !important;
 }
 
-.validSubmit{
+.validSubmit {
   background: green !important;
 }
 
-.login-form input{
-  outline:none;
-  font-size:20px;
+.login-form input {
+  outline: none;
+  font-size: 20px;
   font-weight: 800;
   padding: 10px;
   border-radius: 7px;
-  border:none;
+  border: none;
   color: var(--color-main-white);
 }
 
-.text-fields input{
+.text-fields input {
   background: var(--color-background);
   border: 2px solid var(--color-stroke)
 }
 
-.text-fields input:focus{
+.text-fields input:focus {
   border: 2px solid var(--color-main)
 }
 
-.submit-field input{
-  width:100%;
+.submit-field input {
+  width: 100%;
   background: var(--color-main);
   color: var(--color-main-white)
 }
 
-.submit-field input:hover{
+.submit-field input:hover {
   filter: brightness(120%);
   cursor: pointer;
 }
-
-
 
 
 </style>

89b46aaab44bbfdf167e4c35f9d8e093fd4ea652
Udo Zucker <email@udozucker.de>
2023-02-09 16:25:12 +0100

    added help icons

--- frontend/src/components/views/RegisterView/RegisterView.vue
+++ frontend/src/components/views/RegisterView/RegisterView.vue
@@ -1,23 +1,44 @@
 <template>
   <div class="container">
-    <Card>
+    <Card class="container-card">
       <div class="content">
 
         <div class="header">
-          <div class="title">{{ $t('message.register-view.title') }}</div>
+          <div class="title">{{ $t('message.register-view.title') }}
+            <span class="material-icons md-dark icon" @mouseenter="showTip = true" @mouseleave="showTip = false"> info </span>
+            <transition name="fade">
+              <Card class="card" v-if="showTip" @mouseenter="showTip = true" @mouseleave="showTip = false">
+                {{ $t('message.register-view.requirements') }}
+              </Card>
+            </transition>
+          </div>
+
+
           <div class="subtitle">{{ $t('message.register-view.subtitle') }}</div>
         </div>
 
         <div class="login-form">
-            <div class="error" v-show="error">{{errorMessage}}</div>
-            <div class="text-fields">
-              <input v-bind:class="{'valid': usernameInput(username), 'errorInput':error}" class="username" @input="usernameInput" @click="() => error = false" v-model="username" placeholder="Username"/>
-              <input v-bind:class="{'valid': passwordInput(passwordInput), 'errorInput':error}" class="password" @input="passwordInput" @click="() => error = false" v-model="password" placeholder="Password" type="password"/>
-              <input v-bind:class="{'valid': passwordRepeatedInput(passwordRepeated), 'errorInput':error}" class="passwordRepeated" @input="passwordRepeatedInput" @click="() => error = false" v-model="passwordRepeated" placeholder="Re-enter Password" type="password"/>
+          <div class="error" v-show="error">{{ errorMessage }}</div>
+          <div class="text-fields">
+            <input v-bind:class="{'valid': usernameInput(username), 'errorInput':error}" class="username"
+                   @input="usernameInput" @click="() => error = false" v-model="username" placeholder="Username"/>
+            <div style="position:relative">
+               <input v-bind:class="{'valid': passwordInput(passwordInput), 'errorInput':error}" class="password"
+                   @input="passwordInput" @click="() => error = false" v-model="password" placeholder="Password"
+                   :type="inputTypePassword"/>
+            <span class="material-icons md-dark passwordIcon" @click="toggleVisibilityPassword">{{visiblePassword}}</span>
             </div>
-            <div class="submit-field">
-              <input v-bind:class="{'validSubmit': validInput, 'errorSubmit':error}" ref="submit" @click="tryRegisterSubmit" type="button" value="Create account">
-            </div>
+             <div style="position:relative">
+               <input v-bind:class="{'valid': passwordRepeatedInput(passwordRepeated), 'errorInput':error}"
+                   class="passwordRepeated" @input="passwordRepeatedInput" @click="() => error = false"
+                   v-model="passwordRepeated" placeholder="Re-enter Password" :type="inputTypePasswordRepeated"/>
+            <span class="material-icons md-dark passwordIcon" @click="toggleVisibilityPasswordRepeated">{{visiblePasswordRepeated}}</span>
+             </div>
+          </div>
+          <div class="submit-field">
+            <input v-bind:class="{'validSubmit': validInput, 'errorSubmit':error}" ref="submit"
+                   @click="tryRegisterSubmit" type="button" value="Create account">
+          </div>
         </div>
       </div>
     </Card>
@@ -44,21 +65,26 @@ export default defineComponent({
       password: "",
       passwordRepeated: "",
       error: false,
-      errorMessage: "Something went wrong!"
+      errorMessage: "Something went wrong!",
+      showTip: false,
+      visiblePassword: "visibility",
+      inputTypePassword: "password",
+      visiblePasswordRepeated: "visibility",
+      inputTypePasswordRepeated: "password",
     };
   },
   methods: {
-    usernameInput(event:any){
+    usernameInput(event: any) {
       let isValid = validateUsername(this.username)
       this.withValidInput[0] = isValid;
       return isValid
     },
-    passwordInput(event:any){
+    passwordInput(event: any) {
       let isValid = validatePassword(this.password);
       this.withValidInput[1] = isValid;
       return isValid;
     },
-    passwordRepeatedInput(event:any){
+    passwordRepeatedInput(event: any) {
       let isValid = this.passwordRepeated === this.password && this.withValidInput[1];
       this.withValidInput[2] = isValid;
       return isValid;
@@ -67,19 +93,19 @@ export default defineComponent({
       this.username = ""
       this.password = ""
       this.passwordRepeated = ""
-      for (let i=0; i < this.withValidInput.length; i++) {
+      for (let i = 0; i < this.withValidInput.length; i++) {
         this.withValidInput[i] = false
       }
     },
     async tryRegisterSubmit() {
 
       if (!validateUsername(this.username) || !validatePassword(this.password)
-        ||  this.password != this.passwordRepeated) {
-          this.error = true;
-          this.errorMessage = this.$t('message.register-view.errors.provided-credentials-wrong')
-          this.resetInputFields()
-          return;
-        }
+          || this.password != this.passwordRepeated) {
+        this.error = true;
+        this.errorMessage = this.$t('message.register-view.errors.provided-credentials-wrong')
+        this.resetInputFields()
+        return;
+      }
 
       const response = await register(this.username, this.password)
       if (response.error != null) {
@@ -89,11 +115,19 @@ export default defineComponent({
         return;
       }
       await router.push('/')
+    },
+    toggleVisibilityPassword(){
+      this.inputTypePassword = this.inputTypePassword === "password"  ? "text" : "password"
+      this.visiblePassword = this.visiblePassword === "visibility_off" ? "visibility": "visibility_off"
+    },
+    toggleVisibilityPasswordRepeated(){
+      this.inputTypePasswordRepeated = this.inputTypePasswordRepeated === "password"  ? "text" : "password"
+      this.visiblePasswordRepeated= this.visiblePasswordRepeated === "visibility_off" ? "visibility": "visibility_off"
     }
   },
   computed: {
     validInput() {
-      for (let i=0; i < this.withValidInput.length; i++) {
+      for (let i = 0; i < this.withValidInput.length; i++) {
         if (!this.withValidInput[i]) {
           return false
         }
@@ -105,114 +139,161 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.errorInput{
-  border-color: var(--color-close-button) !important;
-  background: none;
-  color: var(--color-close-button)!important;
+
+.card{
+  position: absolute;
+  margin:0;
+  top:82%;
+  left:95%;
+  width:95%;
+  z-index: 90;
+  background: var(--color-topbar);
+  font-weight:400;
+  font-size: 16px;
+  white-space: break-spaces;
 }
 
-.errorSubmit{
+.passwordIcon {
+  float: right;
+  position: absolute;
+  right: 18px;
+  top: 13px;
+  z-index: 90;
+  color: var(--color-main-white);
+  width: 24px;
+  height: 24px;
+  user-select: none;
+}
+
+.passwordIcon:hover{
+  cursor:pointer;
+}
+
+.icon{
+  color:var(--color-main);
+  width: auto;
+  text-align: right;
+  margin:0;
+}
+
+.errorInput {
+  border-color: var(--color-close-button) !important;
+  background: none;
+  color: var(--color-close-button) !important;
+}
+
+.errorSubmit {
   background: var(--color-close-button) !important;
 }
 
-.error{
-  font-size:18px;
+.error {
+  font-size: 18px;
   font-weight: 500;
   border: 2px solid var(--color-close-button);
-  color:var(--color-close-button);
+  color: var(--color-close-button);
   padding: 10px;
   border-radius: 7px;
 }
 
-.container{
-  height:100%;
+.container {
+  height: 100%;
   align-items: center;
   display: flex;
   justify-content: center;
 }
 
-.tip{
-  background:var(--color-auth-form-background);
+.container-card {
+  background: var(--color-auth-form-background);
   border: none;
   padding: 0;
-  margin:0;
+  margin: 0;
 }
 
 .content {
   width: min(70vw, 325px);
   padding: 30px;
-  display:grid;
+  display: grid;
   grid-template-rows: auto auto;
-  gap:30px;
+  gap: 30px;
 }
 
-.header{
+.header {
   text-align: left;
-  display:grid;
+  display: grid;
   gap: 7px;
 }
 
-.title{
+.title {
   font-size: 22px;
   font-weight: 800;
+  display:flex;
+  justify-content: space-between;
+  position: relative;
 }
 
-.subtitle{
-  font-size:20px;
+.subtitle {
+  font-size: 20px;
   font-weight: 700;
   color: var(--color-stroke)
 }
 
-.login-form{
+.login-form {
   display: grid;
   grid-template-rows: auto;
   gap: 30px;
 }
 
-.text-fields{
-  display:grid;
-  gap:15px;
+.text-fields {
+  display: grid;
+  gap: 15px;
 }
 
-.valid{
+.valid {
   border: 2px solid var(--color-running) !important;
 }
 
-.validSubmit{
+.validSubmit {
   background: var(--color-running) !important;
 }
 
-.login-form input{
-  outline:none;
-  font-size:20px;
+.login-form input {
+  outline: none;
+  font-size: 20px;
   font-weight: 800;
   padding: 10px;
   border-radius: 7px;
-  border:none;
+  border: none;
 }
 
-.text-fields input{
+.text-fields input {
   background: var(--color-background);
   border: 2px solid var(--color-stroke);
-  color:var(--color-main-white);
+  color: var(--color-main-white);
+
 }
 
-.text-fields input:focus{
+.text-fields input:focus {
   border: 2px solid var(--color-main)
 }
 
-.submit-field input{
-  width:100%;
+.submit-field input {
+  width: 100%;
   background: var(--color-main);
-  color:var(--color-main-white);
+  color: var(--color-main-white);
 }
 
-.submit-field input:hover{
+.submit-field input:hover {
   filter: brightness(120%);
   cursor: pointer;
 }
 
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
 
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 
 
 </style>

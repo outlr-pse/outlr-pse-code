cabf2a71c4bc00cf0377109edc93d7d91e446fc4
Simeon Schrape <ucfoh@student.kit.edu>
2023-02-14 10:57:27 +0100

    some dashboard changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,20 +9,46 @@
 
 import Topbar from "./components/topbar/Topbar.vue";
 import {RouterView} from "vue-router";
+import "vue-router/dist/vue-router";
+import {defineComponent} from "vue";
+import store from "./store";
+import {requestTokenIdentity, storage} from "./api/APIRequests";
 
-export default {
+export default defineComponent({
   data() {
     return {
-      loggedIn: false
+      loggedIn: false,
+      intervalID: null as any
+
     }
   },
-
+  mounted() {
+    this.intervalID = setInterval(this.checkAuth, 1000)
+  },
+  beforeUnmount() {
+    clearInterval(this.intervalID)
+  },
+  methods: {
+    async checkAuth() {
+      if (this.$route.meta.requiresAuth) {
+        console.log("Checking auth")
+        let responseJson = await requestTokenIdentity()
+        if (responseJson != null && "username" in responseJson && "access_token" in responseJson) {
+          await store.dispatch("auth/setAuthenticated", responseJson.username)
+        } else {
+          console.log("Unsetting auth")
+          await store.dispatch("auth/unsetAuthenticated")
+          this.$router.push("/")
+        }
+      }
+    }
+  },
   components: {
     Topbar,
     RouterView
   },
 
-}
+})
 </script>
 
 

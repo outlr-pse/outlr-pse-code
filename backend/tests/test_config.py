8e8061217820d41d1d5235fffd956bff24d44dda
Erik Wu <erik.wu@gmx.de>
2023-03-15 20:11:29 +0100

    test pipeline failing

--- backend/tests/test_config.py
+++ backend/tests/test_config.py
@@ -6,3 +6,4 @@ class TestConfig(unittest.TestCase):
     def test_config(self):
         self.assertIsNotNone(config.jwt_secret)
         self.assertIsNotNone(config.db_url)
+        self.assertTrue(False)

1dc52a2f9f01e9070a4b9016689db22ebfcb9ec7
Bennet Hörmann <bennet.hoermann@student.kit.edu>
2023-03-17 02:00:20 +0100

    Fix lint

--- frontend/src/logic/subspacelogic/SubspaceLogicParser.ts
+++ frontend/src/logic/subspacelogic/SubspaceLogicParser.ts
@@ -1,105 +1,132 @@
-import {SubspaceLogic} from "../../models/subspacelogic/SubspaceLogic";
-import {Token, tokenize, TokenType} from "./SubspaceLogicTokenizer";
-import {Subspace} from "../../models/results/Subspace";
-import {Operator} from "../../models/subspacelogic/Operator";
-import {Operation} from "../../models/subspacelogic/Operation";
-import {Literal} from "../../models/subspacelogic/Literal";
+import type { SubspaceLogic } from '../../models/subspacelogic/SubspaceLogic'
+import { Token, tokenize, TokenType } from './SubspaceLogicTokenizer'
+import { Subspace } from '../../models/results/Subspace'
+import { Operator } from '../../models/subspacelogic/Operator'
+import { Operation } from '../../models/subspacelogic/Operation'
+import { Literal } from '../../models/subspacelogic/Literal'
 
 /**
  * Parses a subspace logic expression
  * The expression must follow this grammar:
  *  Logic -> (Logic) | Literal | Logic Operator Logic Operator Logic ...
- *      where each Operator must be the same
+ *    where each Operator must be the same
  *  Operator -> and | or
  *  Literal -> [ comma separated list of integers ]
  * @throws Error if the expression cannot be tokenized or parsed
  */
 export function parseSubspaceLogic(expression: string): SubspaceLogic {
-    return parseSubspaceLogicTokens(tokenize(expression))
+  return parseSubspaceLogicTokens(tokenize(expression))
 }
 
+/**
+ * Parses a subspace logic expression from a list of tokens
+ * @param tokens Token list (see {@link tokenize})
+ * @throws Error if the expression cannot be parsed
+ */
 export function parseSubspaceLogicTokens(tokens: Token[]): SubspaceLogic {
-    const [logic, _] = parse(tokens, 0, false)
-    // maybe check end here?
-    return logic
+  const [logic, _] = parse(tokens, 0, false)
+  // maybe check end here?
+  return logic
 }
 
+
+/**
+ * Parses a subspace logic expression from a list of tokens
+ * @param tokens Token list
+ * @param begin Index of the first token to parse
+ * @param expectEndScope Whether the expression must end with an end scope token
+ * @returns The parsed expression and the index of the first token after the expression
+ * @throws Error if the expression cannot be parsed
+ */
 function parse(tokens: Token[], begin: number, expectEndScope: boolean): [SubspaceLogic, number] {
-    let operator: Operator | null = null
-    let operands: SubspaceLogic[] = []
+  let operator: Operator | null = null
+  let operands: SubspaceLogic[] = []
 
-    let current = begin
-    while(true) {
+  let current = begin
+  while(true) {
 
-        // Ensure that there are tokens left
-        if (current >= tokens.length)
-            throw new Error("SubspaceLogicParser: Unexpected end of tokens")
-        if (tokens[current][0] == TokenType.EndScope)
-            throw new Error("SubspaceLogicParser: Unexpected end of scope")
+    // Ensure that there are tokens left
+    if (current >= tokens.length)
+      throw new Error("SubspaceLogicParser: Unexpected end of tokens")
+    if (tokens[current][0] == TokenType.EndScope)
+      throw new Error("SubspaceLogicParser: Unexpected end of scope")
 
-        // Read operand
-        let [operandType, _] = tokens[current]
-        current += 1
-        if (operandType == TokenType.BeginScope) {
-            let [operand, end] = parse(tokens, current, true)
-            operands.push(operand)
-            current = end
-        }
-        else if (operandType == TokenType.BeginLit) {
-            let [literal, end] = parseLiteral(tokens, current)
-            operands.push(literal)
-            current = end
-        }
-        else {
-            throw new Error("SubspaceLogicParser: Unexpected token type for operand: " + operandType)
-        }
-
-        // Check if end is reached
-        if (current >= tokens.length) {
-            if (expectEndScope) throw new Error("SubspaceLogicParser: Missing end of scope")
-            break
-        }
-        else if (tokens[current][0] == TokenType.EndScope) {
-            if (!expectEndScope) throw new Error("SubspaceLogicParser: Unexpected end of scope")
-            current += 1
-            break
-        }
-
-        // If not end, read operator
-        let [operatorToken, operatorValue] = tokens[current]
-        current += 1
-        if (operatorToken != TokenType.Text)
-            throw new Error("SubspaceLogicParser: Expected operator, got " + operatorToken)
-        const readOperator = parseOperator(operatorValue)
-        if (operator == null) // First time reading operator
-            operator = readOperator
-        else if (operator != readOperator) // Operator different from previously read
-            throw new Error("SubspaceLogicParser: Found different operators in one scope")
+    // Read operand
+    let [operandType, _] = tokens[current]
+    current += 1
+    if (operandType == TokenType.BeginScope) {
+      let [operand, end] = parse(tokens, current, true)
+      operands.push(operand)
+      current = end
     }
-    if (operands.length == 1)
-        return [operands[0], current]
-    return [new Operation(operator!, operands), current] // operator is not null here
+    else if (operandType == TokenType.BeginLit) {
+      let [literal, end] = parseLiteral(tokens, current)
+      operands.push(literal)
+      current = end
+    }
+    else {
+      throw new Error("SubspaceLogicParser: Unexpected token type for operand: " + operandType)
+    }
+
+    // Check if end is reached
+    if (current >= tokens.length) {
+      if (expectEndScope) throw new Error("SubspaceLogicParser: Missing end of scope")
+      break
+    }
+    else if (tokens[current][0] == TokenType.EndScope) {
+      if (!expectEndScope) throw new Error("SubspaceLogicParser: Unexpected end of scope")
+      current += 1
+      break
+    }
+
+    // If not end, read operator
+    let [operatorToken, operatorValue] = tokens[current]
+    current += 1
+    if (operatorToken != TokenType.Text)
+      throw new Error("SubspaceLogicParser: Expected operator, got " + operatorToken)
+    const readOperator = parseOperator(operatorValue)
+    if (operator == null) // First time reading operator
+      operator = readOperator
+    else if (operator != readOperator) // Operator different from previously read
+      throw new Error("SubspaceLogicParser: Found different operators in one scope")
+  }
+  if (operands.length == 1)
+    return [operands[0], current]
+  return [new Operation(operator!, operands), current] // operator is not null here
 }
 
-function parseLiteral(tokens: Token[], begin: number): [SubspaceLogic, number] {
-    let [tokenType, tokenValue] = tokens[begin]
-    if (tokenType != TokenType.Cols)
-        throw new Error("SubspaceLogicParser: Expected subspace columns, got " + tokenType)
-    if (tokens[begin + 1][0] != TokenType.EndLit)
-        throw new Error("SubspaceLogicParser: Expected end of literal, got " + tokens[begin + 1][0])
+/**
+ * Parses a literal from a list of tokens
+ * @param tokens Token list
+ * @param begin Index of the first token to parse
+ * @returns The parsed literal and the index of the first token after the literal
+ * @throws Error if the literal cannot be parsed
+ */
+function parseLiteral(tokens: Token[], begin: number): [Literal, number] {
+  let [tokenType, tokenValue] = tokens[begin]
+  if (tokenType != TokenType.Cols)
+    throw new Error("SubspaceLogicParser: Expected subspace columns, got " + tokenType)
+  if (tokens[begin + 1][0] != TokenType.EndLit)
+    throw new Error("SubspaceLogicParser: Expected end of literal, got " + tokens[begin + 1][0])
 
-    // tokenValue is the list of column indices
-    return [new Literal(newSubspace(tokenValue)), begin + 2] // 2 tokens were consumed
+  // tokenValue is the list of column indices
+  return [new Literal(newSubspace(tokenValue)), begin + 2] // 2 tokens were consumed
 }
 
+/**
+ * Parses an operator from a string
+ * @param operator Operator as string
+ * @returns The parsed operator (see {@link Operator})
+ * @throws Error if the operator is unknown
+ */
 function parseOperator(operator: string): Operator {
-    switch (operator) {
-        case "and": return Operator.AND
-        case "or": return Operator.OR
-        default: throw new Error("SubspaceLogicParser: Unknown operator: " + operator)
-    }
+  switch (operator) {
+    case "and": return Operator.AND
+    case "or": return Operator.OR
+    default: throw new Error("SubspaceLogicParser: Unknown operator: " + operator)
+  }
 }
 
 function newSubspace(cols: number[]): Subspace {
-    return new Subspace(null, null, cols)
+  return new Subspace(null, null, cols)
 }
--- frontend/src/logic/subspacelogic/SubspaceLogicTokenizer.ts
+++ frontend/src/logic/subspacelogic/SubspaceLogicTokenizer.ts
@@ -1,64 +1,64 @@
 
 export const enum TokenType {
-    BeginLit, EndLit, BeginScope, EndScope, Text, Cols
+  BeginLit, EndLit, BeginScope, EndScope, Text, Cols
 }
 
 export type Token = [TokenType, any?]
 
 export function tokenize(expression: string): Token[] {
-    let tokens: Token[] = []
-    let i = 0
-    while (i < expression.length) {
-        const firstChar = expression.charAt(i)
-        if (firstChar == '[') {
-            i = tokenizeLiteral(expression, i + 1, tokens)
-        } else if (firstChar == '(') {
-            tokens.push([TokenType.BeginScope, undefined])
-            i += 1
-        } else if (firstChar == ')') {
-            tokens.push([TokenType.EndScope, undefined])
-            i += 1
-        } else if (letterRegex.test(firstChar)) {
-            i = tokenizeText(expression, i, tokens)
-        } else if (whitespaceRegex.test(firstChar)) {
-            // Ignore
-            i += 1
-        } else {
-            throw new Error("SubspaceLogicTokenizer: Unexpected character: " + firstChar)
-        }
+  let tokens: Token[] = []
+  let i = 0
+  while (i < expression.length) {
+    const firstChar = expression.charAt(i)
+    if (firstChar == '[') {
+      i = tokenizeLiteral(expression, i + 1, tokens)
+    } else if (firstChar == '(') {
+      tokens.push([TokenType.BeginScope, undefined])
+      i += 1
+    } else if (firstChar == ')') {
+      tokens.push([TokenType.EndScope, undefined])
+      i += 1
+    } else if (letterRegex.test(firstChar)) {
+      i = tokenizeText(expression, i, tokens)
+    } else if (whitespaceRegex.test(firstChar)) {
+      // Ignore
+      i += 1
+    } else {
+      throw new Error("SubspaceLogicTokenizer: Unexpected character: " + firstChar)
     }
-    return tokens
+  }
+  return tokens
 }
 
 function tokenizeLiteral(expression: string, begin: number, tokens: Token[]): number {
-    tokens.push([TokenType.BeginLit, undefined])
-    let end = expression.indexOf(']', begin)
-    if (end == -1) {
-        throw new Error("SubspaceLogicTokenizer: Missing closing bracket ']'")
-    }
-    const split = expression.substring(begin, end)
-        .split(',')
-        .map(str => str.trim())
-    if (split.length == 1 && !nonWhitespaceRegex.test(split[0]))
-        throw new Error("SubspaceLogicTokenizer: Column indices of literals must not be empty")
+  tokens.push([TokenType.BeginLit, undefined])
+  let end = expression.indexOf(']', begin)
+  if (end == -1) {
+    throw new Error("SubspaceLogicTokenizer: Missing closing bracket ']'")
+  }
+  const split = expression.substring(begin, end)
+    .split(',')
+    .map(str => str.trim())
+  if (split.length == 1 && !nonWhitespaceRegex.test(split[0]))
+    throw new Error("SubspaceLogicTokenizer: Column indices of literals must not be empty")
 
-    const cols = split.map(str => parseInt(str))
-    if (cols.some(isNaN))
-        throw new Error("SubspaceLogicTokenizer: Column indices of literals must be integers: " + cols)
+  const cols = split.map(str => parseInt(str))
+  if (cols.some(isNaN))
+    throw new Error("SubspaceLogicTokenizer: Column indices of literals must be integers: " + cols)
 
-    tokens.push([TokenType.Cols, cols])
-    tokens.push([TokenType.EndLit, undefined])
-    return end + 1
+  tokens.push([TokenType.Cols, cols])
+  tokens.push([TokenType.EndLit, undefined])
+  return end + 1
 }
 
 function tokenizeText(expression: string, begin: number, tokens: Token[]): number {
-    let end = begin + 1
-    while (end < expression.length && letterRegex.test(expression.charAt(end))) {
-        end += 1
-    }
-    let word = expression.substring(begin, end)
-    tokens.push([TokenType.Text, word])
-    return end
+  let end = begin + 1
+  while (end < expression.length && letterRegex.test(expression.charAt(end))) {
+    end += 1
+  }
+  let word = expression.substring(begin, end)
+  tokens.push([TokenType.Text, word])
+  return end
 }
 
 const letterRegex = /[a-zA-Z]/

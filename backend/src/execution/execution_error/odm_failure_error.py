90b0b7085dbf9b670f2a1174694ef7f7975ede96
Bennet Hörmann <bennet.hoermann@student.kit.edu>
2023-03-17 16:17:49 +0100

    Add tests for execution errors

--- backend/src/execution/execution_error/odm_failure_error.py
+++ backend/src/execution/execution_error/odm_failure_error.py
@@ -8,11 +8,11 @@ class ODMFailureError(ExecutionError):
         odm_message (str): The message of the exception that was raised during the execution of the ODM
     """
 
-    def __init__(self, odm_message: str):
+    def __init__(self, odm_message: str, odm_name: str):
         self.odm_message = odm_message
-        super().__init__(debug_message=f"ODM failed internally with the following message: {odm_message}")
+        super().__init__(debug_message=f"ODM {odm_name} failed internally with the following message: {odm_message}")
 
     def _to_json(self) -> dict:
         return {
-            "odm_message": self.odm_message
+            "odm_message": self.odm_message,
         }
